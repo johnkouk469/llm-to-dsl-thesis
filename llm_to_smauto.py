62efda0c97cf1c6020ac7b9a5e7f06eea53163d9
johnkouk469 <johnkouk469@gmail.com>
2025-07-15 14:41:44 +0300

    Deprecated, remove generation from yaml

--- llm_to_smauto.py
+++ llm_to_smauto.py
@@ -14,7 +14,6 @@ from langchain_core.prompts import (
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
 from requests import Response
-import yaml
 
 import smauto_api
 import smauto_prompts
@@ -77,41 +76,6 @@ def generate_smauto_model(
         return smauto_model, history
     except Exception as e:
         logger.error("Error generating SmAuto model: %s", e)
-        raise
-
-
-def generate_smauto_model_from_yaml(
-    yaml_file_path: str, history: Optional[List[Tuple[str, str]]] = None
-) -> Tuple[str, List[Tuple[str, str]]]:
-    """
-    Generates an SmAuto model based on the contents of a YAML file.
-
-    Parameters:
-    yaml_file_path (str): The path to the YAML file containing the input data.
-    history (Optional[List[Tuple[str, str]]]): A list to maintain the history
-    of the conversation. Defaults to None.
-
-    Returns:
-    Tuple[str, List[Tuple[str, str]]]: A tuple containing the generated SmAuto
-    model (str) and the updated conversation history (list).
-    """
-    try:
-        yaml_content = read_yaml_file(yaml_file_path)
-        if history is None:
-            history = []
-
-        smauto_model = invoke_model_generation_from_yaml(yaml_content, history)
-        history.append(("user", format_yaml_message(yaml_content)))
-        history.append(("assistant", smauto_model))
-
-        save_model(smauto_model, "smauto_model" + SMAUTO_FILE_NAME_EXTENSION)
-
-        if not validate_model(smauto_model):
-            smauto_model, history = regenerate_invalid_model(smauto_model, history)
-
-        return smauto_model, history
-    except Exception as e:
-        logger.error("Error generating SmAuto model from YAML: %s", e)
         raise
 
 
@@ -283,30 +247,6 @@ def invoke_model_generation(user_utterance: str, history: List[Tuple[str, str]])
     )
 
 
-def invoke_model_generation_from_yaml(
-    yaml_content: Any, history: List[Tuple[str, str]]
-) -> str:
-    """Invokes the language model to generate the SmAuto model based on YAML content."""
-    prompt_template = ChatPromptTemplate.from_messages(
-        [
-            MessagesPlaceholder("system_prompt"),
-            MessagesPlaceholder("history"),
-            ("user", smauto_prompts.CONSTRUCT_SMAUTO_MODEL_FROM_YAML),
-        ]
-    )
-    model_chain = prompt_template | llm | StrOutputParser()
-    logger.info(
-        "Instructing the LLM to generate an SmAuto model based on the YAML content."
-    )
-    return model_chain.invoke(
-        {
-            "system_prompt": smauto_prompts.get_system_prompt(),
-            "history": history,
-            "yaml_content": yaml_content,
-        }
-    )
-
-
 def invoke_model_generation_after_qna(history: List[Tuple[str, str]]) -> str:
     """Invokes the language model to generate the SmAuto model after the Q&A process."""
     prompt_template = ChatPromptTemplate.from_messages(
@@ -416,17 +356,6 @@ def format_user_message(user_utterance: str) -> str:
     )
 
 
-def format_yaml_message(yaml_content: Any) -> str:
-    """Formats the YAML content for the conversation history."""
-    return (
-        HumanMessagePromptTemplate.from_template(
-            smauto_prompts.CONSTRUCT_SMAUTO_MODEL_FROM_YAML
-        )
-        .format(yaml_content=yaml_content)
-        .pretty_repr()
-    )
-
-
 def save_model(model: str, filename: str) -> None:
     """Saves the generated or regenerated model to a file."""
     with open(os.path.join(RESULTS_PATH, filename), "w", encoding="utf-8") as file:
@@ -496,33 +425,6 @@ def extract_validation_message(validation: Response) -> str:
 def strip_code_tags(model: str) -> str:
     """Removes the code prefix and suffix tags from the model."""
     return model.removeprefix(CODE_PREFIX).removesuffix(CODE_SUFFIX)
-
-
-def read_yaml_file(yaml_file_path: str) -> Any:
-    """
-    Reads and parses the content of a YAML file at the given file path.
-
-    Args:
-        yaml_file_path (str): The path to the YAML file.
-
-    Returns:
-        Any: The parsed content of the YAML file.
-
-    Raises:
-        FileNotFoundError: If the specified file does not exist.
-        yaml.YAMLError: If there is an error processing the YAML file.
-    """
-    try:
-        with open(yaml_file_path, "r", encoding="utf-8") as file:
-            config = yaml.safe_load(file)
-            logger.info("Read and parsed the YAML file: %s", config)
-            return config
-    except FileNotFoundError:
-        logger.error("File not found: %s", yaml_file_path)
-        return None
-    except yaml.YAMLError as exc:
-        logger.error("Error reading and parsing YAML file: %s", exc)
-        return None
 
 
 def conversation():
@@ -609,49 +511,20 @@ def main():
         None
     """
 
-    #     for i in range(10):
-    #         utterance = """I'm looking to automate several parts of my smart home. Here's the setup. In the living room, I have a temperature and humidity sensor that publishes data every 30 seconds, a dimmable lamp, and an air purifier that supports both power and mode control. These sensors should use value generators to simulate readings over time, reflecting natural fluctuations in environmental conditions. The temperature readings should be generated using a Gaussian function with a mean of 22, a maximum value of 35, and a sigma of 1, combined with Gaussian noise with a mean of 0 and sigma of 1. The humidity readings should use a Gaussian generator with a mean of 45, a maximum of 70, and sigma of 2, along with Uniform noise between 0 and 1. The dimmable lamp should support a power attribute (on/off) and a brightness attribute represented as a float value ranging from 0.0 to 100.0. The air purifier should support the following modes: "auto", "eco", "turbo", and "off".
-    # In the bedroom, there’s a temperature sensor that publishes data every one minute, a simple on/off lamp, and an air conditioner with adjustable power, temperature, and mode. The bedroom temperature sensor should also simulate readings using a Gaussian distribution with a mean of 20, a maximum value of 30, and sigma of 1, along with Gaussian noise with a mean of 0 and sigma of 1. The air conditioner should support power control, a temperature setting in degrees Celsius, and multiple modes, including "cool", "heat", "dry", "fan", and "off".
-    # All devices communicate using an MQTT broker. This broker is hosted at mqtt.smarthome.local, operates on port 8883, uses SSL for secure connections, and requires authentication with the username casa_nova and the password TooSmart4U@home. For system monitoring, I’m using a Redis broker located at redis.monitor.local, with SSL enabled, running on port 6380, connected to database index 2, and secured with the API key redis-4a9f1e7b3c2d6f8912a3b4c5d678e9f0. The broker should be configured to publish monitoring events and logs using the namespace "casa.nova.universe", with events published to the "epic_events" topic and logs to the "chaotic_logs" topic.
-    # Here’s what I want to automate: When the bedroom temperature drops below 18°C, the lamp should turn on; this should be a continuously active automation. In the living room, if it gets too hot and humid (above 25°C and 60%) and the lamp is off, the air purifier should turn on in auto mode; this condition should be checked every 90 seconds. Once the temperature and humidity return to normal levels (below 24°C and 50%), the purifier should turn off and its automation should stop running automatically. This condition should be checked every 120 seconds. These two automations should be linked so that each one starts the other upon completion, ensuring they remain mutually responsive.
-    # In the bedroom again, if the average temperature over time rises above 26°C and is unstable, the AC should turn on in cool mode at 23°C. This condition should be checked every 180 seconds. Lastly, every night at exactly 10 PM, the living room lamp should automatically turn on at 30% brightness. For the described automations, if a frequency for checking the condition is not explicitly specified, then assume a default of 60 seconds.
-    # I want the name of the model to be "smart_home_env_control" and its version "1.4". The author of the model should be me, Lucas Navarro, with a reference to my email nova@homehub.tech. Also, I want you to come up with an appropriate description for the model.
-    # """
-    #         conversation_history = []
-
-    #         logger.info("Generating model #%d", i)
-
-    #         smauto_model, conversation_history = generate_smauto_model(
-    #             utterance, conversation_history
-    #         )
-
-    #         logger.info("---------------")
-
-    #         with open(
-    #             os.path.join(EXP3_PATH, str(i) + "_smauto_model.auto"),
-    #             "w",
-    #             encoding="utf-8",
-    #         ) as file:
-    #             file.write(strip_code_tags(smauto_model))
-
     while True:
         # Get user input
         print("Choose an option to interact with the SmAuto assistant:")
         print("1. Have a conversation")
-        print("2. Provide a YAML file")
-        print("3. Have converation with feedback")
-        print("4. Exit")
+        print("2. Have converation with feedback")
+        print("3. Exit")
         choice = input("Enter the number of your choice: ")
 
         # Process based on user's choice
         if choice == "1":
             conversation()
         elif choice == "2":
-            file_path = input("Enter the path to the YAML file: ")
-            generate_smauto_model_from_yaml(file_path)
-        elif choice == "3":
             conversation_with_feedback()
-        elif choice == "4":
+        elif choice == "3":
             print("Exiting the program.")
             break
         else:
